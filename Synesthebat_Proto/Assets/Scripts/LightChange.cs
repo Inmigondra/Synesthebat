35eb28423ea6d123c529a7316071862ab72417ee
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2016-10-25 17:52:54 +0200

    UnityAddLightV1

---
+++ Synesthebat_Proto/Assets/Scripts/LightChange.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightChange : MonoBehaviour 
+{
+
+	public Light pointlight;
+	public float intensity = 1.5f;
+	public float range = 150f;
+
+
+	void Start () 
+	{
+		pointlight = GetComponent<Light> ();
+		pointlight.intensity = intensity;
+		pointlight.range = range;
+
+	}
+	
+
+	void Update () 
+	{
+		pointlight.intensity = pointlight.intensity - 0.001f;
+		pointlight.range = pointlight.range - 0.1f;
+
+
+	//	float A = Input.GetAxis ("BoutonA");
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			pointlight.intensity = pointlight.intensity + 0.02f;
+			pointlight.range = pointlight.range + 1f;
+
+		}
+
+	}
+}
---
+++ Synesthebat_Proto/Assets/Scripts/avatar_movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class avatar_movement : MonoBehaviour 
+{
+
+	public float speed = 10f;
+	public float speedrotateH = 100f;
+	public float speedrotateV = 100f;
+
+
+	void Start()
+	{
+
+	}
+
+	void Update () 
+	{
+		float translate = Input.GetAxis ("VerticalG") * speed * Time.deltaTime;
+		float rotateH = Input.GetAxis ("HorizontalG") * speedrotateH * Time.deltaTime;
+		float rotateV = Input.GetAxis ("VerticalD") * speedrotateV * Time.deltaTime;
+
+		transform.Translate (0, 0, translate);
+		transform.Rotate (0, rotateH, 0);
+		transform.Rotate (rotateV, 0, 0);
+
+
+
+
+	}
+}
