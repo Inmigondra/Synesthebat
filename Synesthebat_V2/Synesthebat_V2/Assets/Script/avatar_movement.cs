574a098ceee635cd60897a4f41f3c7cec6939ee3
Inmigondra <inmigondra@live.fr>
2016-11-14 17:17:40 +0100

    NewProject+FMOD

---
+++ Synesthebat_V2/Synesthebat_V2/Assets/Script/avatar_movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class avatar_movement : MonoBehaviour 
+{
+
+	public float speed = 10f;
+
+
+	void Start()
+	{
+
+	}
+
+	void Update () 
+	{
+		float translateX = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+		float translateY = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+
+		transform.Translate (0, 0, translateX);
+		transform.Translate (translateY, 0, 0);
+	}
+}
