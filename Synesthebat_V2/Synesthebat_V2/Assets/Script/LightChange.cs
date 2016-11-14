4a5b0f281e5a673b6c80ed45f306328a41415265
Inmigondra <inmigondra@live.fr>
2016-11-14 19:45:15 +0100

    remise en place pour protoV2
    Son intégré, en attente du fonctionnement des scripts fmod, du design du
    son et du monde

--- Synesthebat_V2/Synesthebat_V2/Assets/Script/LightChange.cs
+++ Synesthebat_V2/Synesthebat_V2/Assets/Script/LightChange.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class LightChange : MonoBehaviour 
 {
-
 	public Light pointlight;
 	public float intensity;
 	public float range ;
@@ -13,10 +12,7 @@ public class LightChange : MonoBehaviour
 		pointlight = GetComponent<Light> ();
 		pointlight.intensity = intensity;
 		pointlight.range = range;
-
 	}
-	
-
 	void Update () 
 	{
 		if (Input.GetMouseButton (0)) {
---
+++ Synesthebat_V2/Synesthebat_V2/Assets/Script/LightParameter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightParameter : MonoBehaviour {
+	/*FMOD.Studio.EventInstance soundInstance;
+	FMOD.Studio.ParameterInstance Luminosite;
+	public float lightValue;
+
+	// Use this for initialization
+	void Start () {
+		
+		soundInstance.getParameter ("Luminosite", out Luminosite);
+		//Luminosite.getValue (out lightValue);
+		Luminosite.setValue(20.0f);
+		Debug.Log (Luminosite);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Luminosite);
+	}*/
+}
